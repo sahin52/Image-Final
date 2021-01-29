939e3e800abaec57076ef5471f294cfeeafaa1d3
Sahin <sahinkasap52@gmail.com>
2021-01-29 13:14:42 +0300

    q3 tamama yakın

--- q3.py
+++ q3.py
@@ -2,11 +2,13 @@ import cv2 as cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread("Dataset3/2.png")
+img = cv2.imread("Dataset3/1.png")
 # plt.imshow(img)
 # plt.show()
 print(img.dtype)
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+plt.show()
 laplacian = cv2.Laplacian(img,cv2.CV_64F)
 sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
 sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
@@ -24,12 +26,15 @@ plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
 plt.show()
 
 print(sobelx.shape)
-sobely = cv2.cvtColor((255 * sobely).astype(np.uint8), cv2.COLOR_RGB2GRAY)
+sobely = ((sobely/21000+0.5)*256).astype(np.uint8)
+sobely = cv2.cvtColor(sobely, cv2.COLOR_RGB2GRAY)
 
 lines = cv2.HoughLinesP(sobely,1,np.pi/180,100,minLineLength=1200,maxLineGap=5)
 for line in lines:
     x1,y1,x2,y2 = line[0]
     cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+    # plt.imshow(img)
+    # plt.show()
 plt.imshow(img)
 plt.show()
 
